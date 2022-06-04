5300d2a0f3dc8dc550d7ea18209fe9750c389d1d
Squishyshyshy <106857457+Squishyshyshy@users.noreply.github.com>
2022-06-04 12:15:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-let express = require('express');
-let app = express();
-let ejs = require('ejs');
-const haikus = require('./haikus.json');
-const port = process.env.PORT || 3000;
-
-app.use(express.static('public'))
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  res.render('index', {haikus: haikus});
-});
-
-app.listen(port);
+const CONFIG = {
+    introTitle: 'Hellu cục cưngg 😍',
+    introDesc: 'Tui nè =)))
+    Hum nai là anni của mình nè =)). Z là đã 2 tháng iu nhau rùi ớ
+    1 ngày đặc biệt tnay tui lại bỏ bạn ở thủ đô còn tui thì vi vu Ninh Bình ăn chơi bốc giời =)))'.
+    btnIntro: '🤤🤤',
+    title: ' Tui cũng khum bít nói gì nữa ý =)))',
+    desc: 'KHS í nma kiểu từ khi iu bạn tui bị bớt sến khá nhìu hehe
+    Nma thật ra là everytime tui thấy bạn tui chỉ mún lao vào hun bạn chụt chụt thuii',
+    btnYes: ' Tui cũng thế',
+    btnNo: 'Tui mún hun bạn chụt chụt chụt chụt chụt',
+    question:'Bạn có iu tui hơm?'.
+    btnReply: 'Đương nhiên là cóoo <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Tui iu bạn rất nhìuuu',
+    messDesc: 'Hẹn bạn iu 2 ngày sau tại tầng 40 nha =)))).',
+    btnAccept: '🤤🤤',
+}
+}
